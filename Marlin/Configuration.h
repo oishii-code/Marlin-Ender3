7a9f545cc6afa0b98587c2b8e3bd5a2297c29f10
Kevin Wong <25396158+oishii-code@users.noreply.github.com>
2019-01-19 19:37:36 -0800

    Changed PLA preheat hotend/bed to 250/50

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1252,8 +1252,8 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 185
-#define PREHEAT_1_TEMP_BED     45
+#define PREHEAT_1_TEMP_HOTEND 205
+#define PREHEAT_1_TEMP_BED     50
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
